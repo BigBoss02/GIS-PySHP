73ae91954779628614015448e4940839800337af
“wildankw123” <wildankhaustara@icloud.com>
2018-10-22 22:09:02 +0700

    menambahkan komentar

--- 1164058_wildankw/soal4.py
+++ 1164058_wildankw/soal4.py
@@ -1,15 +1,10 @@
-import shapefile
-w=shapefile.Writer(shapefile.POINTM)
-w.shapeType
- 
-w.field("kolom1","C") 
-w.field("kolom2","C")
-
-w.record("ngek","satu") 
-w.record("ngok","dua") 
-
-w.point(1,1) 
-w.point(2,2)
-
-w.save("soal4") 
- 
+import shapefile #mengimpor file shape pada python
+w=shapefile.Writer(shapefile.POINTM) #mensetting file dengan jenis point
+w.shapeType #menggunakan shape jenis apa
+w.field("kolom1","C") #membuat atribut dengan nama kolom1 dengan type character
+w.field("kolom2","C") #membuat atribut dengan nama kolom1 dengan type character
+w.record("ngek","satu") #membuat record dengan isi ngek dan satu
+w.record("ngok","dua") #membuat record dengan isi ngok dan dua
+w.point(1,1) #membuat koordinat dengan point
+w.point(2,2) #membuat koordinat dengan jenis point
+w.save("soal4") #menyimpan data dengan nama soal4
