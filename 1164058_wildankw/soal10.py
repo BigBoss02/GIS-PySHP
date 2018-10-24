4b63bc8ab4b2b9fb28281cd27511f0a83675a76b
“wildankw123” <wildankhaustara@icloud.com>
2018-10-24 22:31:22 +0700

    soal10 dan tugas selesai allhamdulillah

---
+++ 1164058_wildankw/soal10.py
@@ -0,0 +1,77 @@
+import shapefile
+w=shapefile.Writer()
+w.shapeType
+
+w.field("kolom1","C")
+w.field("kolom2","C")
+
+w.record("dan","satu")
+w.record("din","dua")
+w.record("dun","tiga")
+w.record("den","empat")
+w.record("don","lima")
+
+
+w.poly(parts=[[
+	[1,3],
+	[2,3], 
+	[2,2],
+	[1,2],
+	[1,3],
+	[2,2],
+	[1,2],
+	[2,3],
+	[1,3]
+]],shapeType=shapefile.POLYLINE)
+
+w.poly(parts=[[
+	[3,3],
+	[4,3], 
+	[4,2],
+	[3,2],
+	[3,3],
+	[4,2],
+	[3,2],
+	[4,3],
+	[3,3]
+]],shapeType=shapefile.POLYLINE)
+
+w.poly(parts=[[
+	[1,1],
+	[2,1], 
+	[2,0],
+	[1,0],
+	[1,1],
+	[2,0],
+	[1,0],
+	[2,1],
+	[1,1]
+]],shapeType=shapefile.POLYLINE)
+
+w.poly(parts=[[
+	[3,1],
+	[4,1], 
+	[4,0],
+	[3,0],
+	[3,1],
+	[4,0],
+	[3,0],
+	[4,1],
+	[3,1]
+]],shapeType=shapefile.POLYLINE)
+
+w.poly(parts=[[
+	[5,3],
+	[6,3], 
+	[6,2],
+	[5,2],
+	[5,3],
+	[6,2],
+	[5,2],
+	[6,3],
+	[5,3]
+]],shapeType=shapefile.POLYLINE)
+
+
+
+w.save("soal10")
